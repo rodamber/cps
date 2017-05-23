6a47929d42454f90dd85abf470b827c3b2969489
rodamber <rodrigoambernardo@gmail.com>
2017-05-23 17:29:01 +0100

    Updated tests.

--- test_shor.py
+++ test_shor.py
@@ -2,7 +2,11 @@ import shor
 
 
 def test_shor():
-    from numpy import prod
+    import numpy as np
 
-    limit = 21
-    assert all(prod(shor.factorize(x)) == x for x in range(2, limit))
+    limit = 100
+    for x in range(2, limit):
+        factors = shor.factorize(x)
+
+        assert np.prod(factors) == x
+        assert all(shor.prime(f) for f in factors)
