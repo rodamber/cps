101b32694956c64dccf17436773002f102a1daa8
rodamber <rodrigoambernardo@gmail.com>
2017-05-24 21:56:08 +0100

    Implemented quantum register.

--- qcomputer.py
+++ qcomputer.py
@@ -1,18 +1,40 @@
 #!/usr/bin/env python3
 
+import linalg as la
+
 
 class Qureg:
     """Represents a quantum register with arbitrary width (number of
     qubits)."""
 
-    def __init__(self, initval, width):
-        pass
+    def __init__(self, width=1, initval=0):
+        # Start in the state |initval>
+        self.amplitudes = la.vec([0] * 2**width)
+        self.amplitudes[initval] = 1
 
     def measure(self):
-        pass
+        from numpy.random import choice
+
+        bit = choice(len(self.amplitudes), p=self.amplitudes)
+
+        self.amplitudes.array.fill(0)  # Every amplitude becomes zero
+        self.amplitudes[bit] = 1
+
+        return bit
+
+    def __repr__(self):
+        s = ""
+        for i, amp in enumerate(self.amplitudes):
+            s += "{:.4g} |{}> + ".format(amp, i)
+        return s[:-3]
 
 
 # 1-qubit gates
+
+# add parameter to apply the gate several times?
+
+# The sum of the squares of the amplitudes must sum to one.
+# assert self.amplitudes.norm == 1
 
 
 def phase_shift(qureg, target, gamma):
