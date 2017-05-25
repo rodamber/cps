918f88c46a7ebbdbacb24e762e0dbb82547ee6b2
rodamber <rodrigoambernardo@gmail.com>
2017-05-25 21:04:36 +0100

    First try on shor.

--- quantum.py
+++ quantum.py
@@ -2,36 +2,42 @@
 
 import math
 import cmath
-from collections import defaultdict
 
 
 class QuMem:
     """Represents quantum memory with arbitrary width (number of qubits)."""
 
     def __init__(self, t, n):
-        self.t = t
-        self.n = n
+        self.t = t  # left width
+        self.n = n  # right width
 
-        self.amplitudes = {}
+        self.amplitudes = []
+        self.left = []
+        self.right = []
 
-        for i in range(2**t):
-            for j in range(2**n):
-                self.amplitudes[(i, j)] = 0
-
-        self.amplitudes[(0, 0)] = 1
+        for left in range(2**t):
+            for right in range(2**n):
+                self.amplitudes.append(0)
+                self.left.append(left)
+                self.right.append(right)
+        self.amplitudes[0] = 1
 
     def measure(self):
-        assert False, "measure not implemented"
+        from numpy import random
+        return random.choice(
+            self.left, p=list(map(lambda x: abs(x)**2, self.amplitudes)))
 
     def __len__(self):
         return len(self.amplitudes)
 
+    def __iter__(self):
+        for x in zip(self.amplitudes, self.left, self.right):
+            yield x
+
     def __repr__(self):
         s = ""
-
-        for k, v in sorted(self.amplitudes.items()):
-            i, j = k
-            s += "{} |{},{}> + ".format(v, i, j)
+        for ampl, left, right in self:
+            s += "{:.4f} |{},{}> + ".format(ampl, left, right)
         return s[:-3]
 
     def hadamard(self):
@@ -39,39 +45,68 @@ class QuMem:
         of the memory. This simulates that by (1) setting the amplitude of all
         the states where the last n bits are different from zero, and (2)
         giving the same amplitude to the other states."""
+
         # FIXME: Review the documentation.
-        for k, v in self.amplitudes.items():
-            _, j = k
-            if j == 0:
-                self.amplitudes[k] = 1 / math.sqrt(2**self.t)
-            else:
-                self.amplitudes[k] = 0
+        for i, (_, left, right) in enumerate(self):
+            if right == 0:
+                self.amplitudes[i] = 1 / math.sqrt(2**self.t)
         return self
 
     def mod_exp(self, x, N):
-        amps = defaultdict(int)
-
-        for k, v in self.amplitudes.items():
-            i, _ = k
-            amps[(i, pow(x, i, N))] += v
-        self.amplitudes = amps
-
+        for i, (amp, left, right) in enumerate(self):
+            self.right[i] = pow(x, left, N)
         return self
 
     def qft(self):
-        pass
+        new_amplitudes = []
+        N = 2**self.t
+
+        for k, _ in enumerate(self):
+            s = 0
+            for j in range(N):
+                wjk = cmath.exp(2 * math.pi * 1j * j * k / N)
+                s += wjk * self.amplitudes[j]
+            new_amplitudes.append(s / math.sqrt(N))
+        self.amplitudes = new_amplitudes
+        return self
+
+
+def denominator(x, qmax):
+    """Finds the denominator q of the best rational approximation p/q for x
+    with q < qmax."""
+    y = x
+    q0, q1, q2 = 0, 1, 0
+
+    while True:
+        z = y - math.floor(y)  # decimal part of y
+        if z < 0.5 / qmax**2:
+            return q1
+
+        y = 1 / z
+        q2 = math.floor(y) * q1 + q0
+
+        if q2 >= qmax:
+            return q1
+
+        q0, q1 = q1, q2
 
 
 def shor(N, x):
-    n = math.ceil(math.log(N, 2))
+    n = N.bit_length()
     t = math.ceil(math.log(N**2, 2))  # s.t. N**2 <= 2**t < 2 * N**2
 
-    mem = QuMem(t, n)
+    measured = QuMem(t, n).hadamard().mod_exp(x, N).qft().measure()
 
-    mem.hadamard(t)
-    mem.mod_exp(x, N)
-    # mem.inv_qft()
-    # mem.measure()
+    if measured == 0:
+        print("Measured 0. Trying again.")
+        shor(N, x)
+    else:
+        print("Measured", measured)
 
-    print(t, n)
-    return mem
+    r = denominator(measured / 2**t, N)
+
+    if pow(x, r, N) == 1:
+        print("Found period:", x, "^", r, "mod", N, "= 1")
+        return r
+    else:
+        shor(N, x)
