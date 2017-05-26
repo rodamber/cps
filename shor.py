65b6f0ed9fbdb21cf4735b0e87ce6cfb13c949d2
rodamber <rodrigoambernardo@gmail.com>
2017-05-26 17:27:12 +0100

    First version of last version.

--- shor.py
+++ shor.py
@@ -1,69 +1,105 @@
 #!/usr/bin/env python3
+"""Simulation of Shor's algorithm for order finding, with an application to
+integer factorization."""
 
 import cmath
 import math
+import numpy as np
 import random
 
 
 class QuMem:
+    """Representation of the memory of the quantum computer."""
+
     def __init__(self, t, n):
-        self.t = t  # left width
-        self.n = n  # right width
+        """Initialize the memory. For Shor's algorithm we have t + n qubits,
+        where t is such that N^2 <= 2^t < 2N^2 holds.
 
+        The memory is represented by explicitly saving all the 2^(t+n) possible
+        states and their corresponding amplitudes.
+        """
+
+        # The amplitudes and the states are represented by three lists where,
+        # for each i, 0 <= i < 2^(t+n), amplitudes[i] is the amplitude of the
+        # state |fst[i], lst[i]>.
         self.amplitudes = []
-        self.left = []
-        self.right = []
+        self.fst = []  # Quantum state of the first t qubits.
+        self.lst = []  # Quantum state of the last n qubits.
 
-        for left in range(2**t):
-            for right in range(2**n):
+        self.t = t  # fst width
+        self.n = n  # lst width
+
+        # Populate the quantum state lists.
+        for fst in range(2**t):
+            for lst in range(2**n):
                 self.amplitudes.append(0)
-                self.left.append(left)
-                self.right.append(right)
+                self.fst.append(fst)
+                self.lst.append(lst)
+
+        assert len(self.amplitudes) == 2**(t + n)
+
+        # Initialize the memory to the state |0, 0>.
         self.amplitudes[0] = 1
 
     def measure(self):
-        from numpy import random
-        return random.choice(
-            self.left, p=list(map(lambda x: abs(x)**2, self.amplitudes)))
+        """Measure the first t bits of the memory. Simulated by making a
+        weighted random choice of one of the possible states. The weights are
+        the squares of the absolute values of their amplitudes."""
+        return np.random.choice(
+            self.fst, p=list(map(lambda x: abs(x)**2, self.amplitudes)))
 
     def __len__(self):
+        """Equal to 2^(t+n). This is here for convenience."""
         return len(self.amplitudes)
 
     def __iter__(self):
-        for x in zip(self.amplitudes, self.left, self.right):
+        """Iterator of the quantum state. This is here for convenience."""
+        for x in zip(self.amplitudes, self.fst, self.lst):
             yield x
 
     def __repr__(self):
+        """Represented as a linear combination, a0 |0, 0> + a1 |0, 1> + ... ,
+        of all the possible states."""
         s = ""
-        for ampl, left, right in self:
-            s += "{:.4f} |{},{}> + ".format(ampl, left, right)
+        for ampl, fst, lst in self:
+            s += "{:.4f} |{},{}> + ".format(ampl, fst, lst)
         return s[:-3]
 
-    def hadamard(self):
-        for i, (_, left, right) in enumerate(self):
-            if right == 0:
-                self.amplitudes[i] = 1 / math.sqrt(2**self.t)
-        return self
 
-    def mod_exp(self, x, N):
-        for i, (amp, left, right) in enumerate(self):
-            self.right[i] = pow(x, left, N)
-        return self
+def hadamard(mem):
+    """Apply the Hadamard gate to the first t qubits. After this
+    application, the memory is in a quantum superposition where the
+    measuring probability is equidistributed between the first t qubits."""
+    for i, (_, fst, lst) in enumerate(mem):
+        if lst == 0:  # The last n qubits remain in state |0>
+            mem.amplitudes[i] = 1 / math.sqrt(2**mem.t)
+    return mem
 
-    def qft(self):
-        new_amplitudes = []
-        N = 2**self.t
 
-        w__ = cmath.exp(2 * math.pi * 1j / N)
-        for k, _ in enumerate(self):
-            w_k = w__**k
-            s = 0
-            for j in range(N):
-                wjk = w_k**j
-                s += wjk * self.amplitudes[j]
-            new_amplitudes.append(s / math.sqrt(N))
-        self.amplitudes = new_amplitudes
-        return self
+def mod_exp(mem, x, N):
+    """Apply the operator |j, k> |-> |j, k + x^j mod N>."""
+    for i, (_, fst, lst) in enumerate(mem):
+        mem.lst[i] = pow(x, fst, N)
+    return mem
+
+
+def qft(mem):
+    """Apply quantum Fourier transform to the first t qubits."""
+    new_amplitudes = []
+    N = 2**mem.t
+
+    # Calculate root of unity step by step, as complex exponentiation seems to
+    # be expensive.
+    w__ = cmath.exp(2 * math.pi * 1j / N)
+
+    for k, _ in enumerate(mem):
+        s = 0
+        for j in range(N):
+            wjk = w__**(j * k)
+            s += wjk * mem.amplitudes[j]
+        new_amplitudes.append(s / math.sqrt(N))
+    mem.amplitudes = new_amplitudes
+    return mem
 
 
 def denominator(x, qmax):
@@ -87,14 +123,21 @@ def denominator(x, qmax):
 
 
 def shor(N, a):
+    assert 1 < a < N
+
     while True:
         n = N.bit_length()
-        t = math.ceil(math.log(N**2, 2))  # s.t. N**2 <= 2**t < 2 * N**2
+        t = math.ceil(math.log(N**2, 2))  # s.t. N^2 <= 2^t < 2N^2
 
-        measure = QuMem(t, n).hadamard().mod_exp(a, N).qft().measure()
+        mem = QuMem(t, n)
+        hadamard(mem)
+        mod_exp(mem, a, N)
+        qft(mem)
+        measure = mem.measure()
 
         if measure == 0:
             print("| measured zero, trying again ...")
+            print("|-----------------------------------------------")
         else:
             c = measure / 2**t
             q = denominator(c, N)
@@ -102,20 +145,15 @@ def shor(N, a):
             print("| measured {}, approximation for {} is {}/{}"
                   .format(measure, c, p, q))
 
-            # FIXME: Is this polinomial in n?
-            print("| trying multiples of {} ...".format(q))
-            for i in range(1, math.ceil(N / q)):
-                mod = pow(a, i * q, N)
+            mod = pow(a, q, N)
+            print("| {}^{} mod {} = {}".format(a, q, N, mod))
 
-                print("| {}^{} mod {} = {}".format(a, i * q, N, mod))
-
-                if mod == 1:
-                    print("| period of f(x) = {}^x mod {} is {}".format(
-                        a, N, i * q))
-                    return i * q
-
-            print("| failed to find period")
-            return None
+            if mod == 1:
+                print("| order is {}".format(q))
+                return q
+            else:
+                print("| failed to find the order")
+                return None
 
 
 def prime(n):
@@ -162,26 +200,36 @@ def factorize(N):
             print("| picked random a =", a)
 
             if d != 1:
-                print("| got lucky, {} = {} * {}".format(N, d, N // d))
-                print("| repeating ...")
+                print("| got lucky, {} = {} * {}, trying again...".format(
+                    N, d, N // d))
+                print("|---------------------------------------------")
             else:
                 r = shor(N, a)
                 if r is None:
                     print("| trying again ...")
+                    print("|-----------------------------------------------")
                     continue
 
                 y = r // 2
                 if r % 2 == 1:
-                    print("| period {} is odd, trying again ...".format(r))
+                    print("| order {} is odd, trying again ...".format(r))
+                    print("|-----------------------------------------------")
                 elif not 1 < y < N - 1:
                     print("| 1 < {} < {} - 1 is false, trying again".format(
                         y, N))
+                    print("|-----------------------------------------------")
                 else:
                     factor = max(math.gcd(y - 1, N), math.gcd(y + 1, N))
 
-                    print("factor is one, trying again ...")
-                    continue
+                    if factor == 1:
+                        print("| factor is one, trying again ...")
+                        print("|---------------------------------------------")
+                    else:
+                        print("| found factor: {} = {} * {}".format(
+                            N, factor, N // factor))
+                        return factor
 
-                    print("| found factor: {} = {} * {}".format(
-                        N, factor, N // factor))
-                    return factor
+
+if __name__ == '__main__':
+    import sys
+    print(factorize(int(sys.argv[1])))
